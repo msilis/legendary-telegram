874652bd6c8b6c4ba9657067890f0b0bcf92129e
msilis <mikssilis@gmail.com>
2023-03-05 20:50:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL);
+mongoose.set('strictQuery', true);
+const db = mongoose.connection;
+db.on('error', (error)=>console.error(error));
+db.once('open', ()=>console.log("Connected to database."));
+
+const groupRouter = require('./routes/groupClassRoutes');
+
+app.use("/groupIdeas", groupRouter);
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.listen(port, ()=> console.log(`Listening on ${port}`));
