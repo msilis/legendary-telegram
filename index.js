13e727181a2f18ac5e37904331160231193c8da3
msilis <mikssilis@gmail.com>
2023-06-06 10:39:36 +0100

    updated cors origin

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://10.128.172.126:5173",
     "http://127.0.0.1:8080",
+    "https://curious-maamoul-a56afb.netlify.app/",
   ],
   credentials: true,
 };
